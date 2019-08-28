25b5b2f67794f950ccf9a5e9fc8742c90b9b9eb2
neohotsauce <46462510+neohotsauce@users.noreply.github.com>
2019-08-28 22:47:14 +0500

    set up react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  useState(() => {
+    console.log('lamo');
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload. lmao
         </p>
         <a
           className="App-link"
